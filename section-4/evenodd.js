30b082949b4c61702fa9fcb265795d92e5632ee6
jeevansiddu <jeevansiddusb@gmail.com>
2023-07-08 12:43:27 +0530

    new course

--- section-4/evenodd.js
+++
@@ -1,8 +0,0 @@
-function showNumbers(limit){
-    for(let i=0;i<limit;i++){
-        let k= i%2==0?"EVEN":"ODD";
-        console.log(`${i} "${k}"`);
-    }
-}
-
-showNumbers(10);
--- section-4/isLandscape.js
+++
@@ -1,5 +0,0 @@
-function isLandscape(width,height){
-    return (width>height);
-}
-
-console.log(isLandscape(400,300));
