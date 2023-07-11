f7c468ba75e5553db6766598a64cbe16388140b1
jeevansiddu <jeevansiddusb@gmail.com>
2023-07-11 10:18:01 +0530

    new branch

--- section-3/swap.js
+++
@@ -1,7 +0,0 @@
-let a = 5;
-let b=10;
-console.log(a+" "+b);
-let swap=a;
-a=b;
-b=swap;
-console.log(a+" "+b);
--- section-4/max.js
+++
@@ -1,5 +0,0 @@
-function max(a,b){
-    if(a>b) return a;
-    return b;
-}
-console.log(`Max is ${max(15,10)}`)
