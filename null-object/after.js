c682dc11320002c99d50ef5b53d8e61108e31ccb
jeevansiddu <jeevansiddusb@gmail.com>
2023-07-11 09:55:35 +0530

    check

---
+++ null-object/after.js
@@ -0,0 +1,43 @@
+class Food {
+  constructor(name, price, id) {
+    this.name = name;
+    this.price = price;
+    this.id = id;
+  }
+  isAffordable() {
+    return this.price < 200;
+  }
+}
+
+class NullFood {
+  constructor() {
+    this.name = "idly";
+    this.price = 80;
+    this.id = 1;
+  }
+  isAffordable() {
+    return this.price < 200;
+  }
+}
+
+const foods = [new Food("pizza", "200", 11), new Food("burger", 90, 12)];
+
+function getFood(id) {
+  return foods.find((food) => {
+    if (food.id == id) {
+      return food;
+    }
+  });
+}
+function printFood(no) {
+  let food = getFood(no);
+  food = food == null ? new NullFood() : food; //here we check whether the food is valid if not valid we automatically
+  //associate it with the null object so rest it will take care by itself
+  console.log(food.name);
+
+  if (food.isAffordable()) {
+    console.log("Affordable");
+  } else {
+    console.log("Not affordable");
+  }
+}
